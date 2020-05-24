37e7cd6e57791e1c63054dbd2c1d23996894eff6
Henry-Ning <henryhuning@gmail.com>
2020-05-24 09:42:47 -0700

    157. JSON Parsing and Dynamic Types 16min

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:clima/services/location.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -17,11 +18,20 @@ class _LoadingScreenState extends State<LoadingScreen> {
 
   void getData() async {
     http.Response response = await http.get(
-        'https://samples.openweathermap.org/data/2.5/weather?1at=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22'
+        'https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b3%C3%B8761fae22'
     );
     if (response.statusCode == 200) {
       String data = response.body;
-      print(data);
+
+      var decodedData = jsonDecode(data);
+
+      double temp = decodedData['main']['temp'];
+      int condition = decodedData['weather'][0]['id'];
+      String cityName = decodedData['name'];
+
+      print(temp);
+      print(condition);
+      print(cityName);
     } else {
       print(response.statusCode);
     }
