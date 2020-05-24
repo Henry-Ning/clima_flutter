6e066f056eac50a51ea4f98182177b5868e72480
Henry-Ning <henryhuning@gmail.com>
2020-05-24 12:21:36 -0700

    159. Showing a Spinner While the User Waits 6min

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:clima/services/location.dart';
 import 'package:clima/services/networking.dart';
+import 'location_screen.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 const apiKey = 'deb7d3fc769b835b244badb477dd5766';
 
@@ -29,8 +31,10 @@ class _LoadingScreenState extends State<LoadingScreen> {
 
     NetworkHelper networkHelper = NetworkHelper('https://samples.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
     var weatherData = await networkHelper.getData();
-    String cityName = weatherData['name'];
-    print(cityName);
+
+    Navigator.push(context, MaterialPageRoute(builder: (context) {
+      return LocationScreen();
+    } ));
   }
 
 
@@ -40,12 +44,9 @@ class _LoadingScreenState extends State<LoadingScreen> {
     getLocationData();
     return Scaffold(
       body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            getLocationData();
-            //Get the current location
-          },
-          child: Text('Get Location'),
+        child: SpinKitDoubleBounce(
+          color: Colors.white,
+          size: 100,
         ),
       ),
     );
