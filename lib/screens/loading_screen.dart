984b05ce795d261756e1511d2da41d585aa72c53
Henry-Ning <henryhuning@gmail.com>
2020-05-24 10:44:34 -0700

    158. Getting Actual Weather Data from the OpenWeatherMap API 10min

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:clima/services/location.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
+import 'package:clima/services/networking.dart';
+
+const apiKey = 'deb7d3fc769b835b244badb477dd5766';
+
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -9,49 +11,38 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
-  void getLocation() async {
-    Location location = Location();
-    await location.getCurrentLocation();
-    print('Longitude is: ${location.longitude}');
-    print(location.latitude);
-  }
-
-  void getData() async {
-    http.Response response = await http.get(
-        'https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b3%C3%B8761fae22'
-    );
-    if (response.statusCode == 200) {
-      String data = response.body;
-
-      var decodedData = jsonDecode(data);
-
-      double temp = decodedData['main']['temp'];
-      int condition = decodedData['weather'][0]['id'];
-      String cityName = decodedData['name'];
-
-      print(temp);
-      print(condition);
-      print(cityName);
-    } else {
-      print(response.statusCode);
-    }
-  }
+  double latitude;
+  double longitude;
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    getLocation();
   }
+
+  void getLocationData() async {
+    Location location = Location();
+    await location.getCurrentLocation();
+
+    latitude = location.latitude;
+    longitude = location.longitude;
+
+    NetworkHelper networkHelper = NetworkHelper('https://samples.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
+    var weatherData = await networkHelper.getData();
+    String cityName = weatherData['name'];
+    print(cityName);
+  }
+
+
 
   @override
   Widget build(BuildContext context) {
-    getData();
+    getLocationData();
     return Scaffold(
       body: Center(
         child: RaisedButton(
           onPressed: () {
-            getLocation();
+            getLocationData();
             //Get the current location
           },
           child: Text('Get Location'),
