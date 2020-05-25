ed651c4bab0001ab5e9e2b9e76f7f32fc8a0d069
Henry-Ning <henryhuning@gmail.com>
2020-05-25 08:04:31 -0700

    160. Passing Data to a State Object 11min

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -6,15 +6,12 @@ import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 const apiKey = 'deb7d3fc769b835b244badb477dd5766';
 
-
 class LoadingScreen extends StatefulWidget {
   @override
   _LoadingScreenState createState() => _LoadingScreenState();
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
-  double latitude;
-  double longitude;
 
   @override
   void initState() {
@@ -26,18 +23,16 @@ class _LoadingScreenState extends State<LoadingScreen> {
     Location location = Location();
     await location.getCurrentLocation();
 
-    latitude = location.latitude;
-    longitude = location.longitude;
-
-    NetworkHelper networkHelper = NetworkHelper('https://samples.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
+    NetworkHelper networkHelper = NetworkHelper(
+        'https://samples.openweathermap.org/data/2.5/weather?lat=${location.latitude}&lon=${location.longitude}&appid=$apiKey');
     var weatherData = await networkHelper.getData();
 
     Navigator.push(context, MaterialPageRoute(builder: (context) {
-      return LocationScreen();
-    } ));
+      return LocationScreen(
+        locationWeather: weatherData,
+      );
+    }));
   }
-
-
 
   @override
   Widget build(BuildContext context) {
